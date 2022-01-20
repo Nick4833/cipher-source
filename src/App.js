bc74d0c2b65686813f5a74736c1184c141ce063e
unknown <lucas.lucas.114200@gmail.com>
2022-01-20 16:26:21 +0630

    Cipher with UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [normalText, setNormalText] = useState('');
+  const [key, setKey] = useState(0);
+  const [cipheredString, setCipheredString] = useState("");
+
+
+  const onChange = (e) =>{
+    const re = /^[0-9\b]+$/;
+
+    // if value is not blank, then test the regex
+
+    if (e.target.value === '' || re.test(e.target.value)) {
+      setKey(parseInt(e.target.value))
+    }
+  }
+
+  const caserCipher = (normalText, key) => {
+    var array = normalText.split("");
+    var cipher = 0;
+    console.log(array);
+    console.log(3+ key);
+    var cipheredString = "";
+    array.forEach(function (item, index) {
+      if (!isNaN(item * 1)) {
+        cipheredString = cipheredString + item;
+      } else {
+        if (item === item.toUpperCase()) {
+          console.log(item.charCodeAt());
+          cipher = ((item.charCodeAt() - 65 + key) % 26) + 65;
+          console.log(cipher);
+          cipheredString = cipheredString + String.fromCharCode(cipher);
+        }
+        if (item === item.toLowerCase()) {
+          cipher = ((item.charCodeAt() + key -97) % 26) + 97;
+          console.log(cipher);
+          cipheredString = cipheredString + String.fromCharCode(cipher);
+        }
+      }
+    });
+    setCipheredString(cipheredString);
+  };
+
+  useEffect(() => {
+    caserCipher(normalText, key);
+
+  }, [normalText, key])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App grid grid-cols-1 justify-items-center items-center h-screen bg-gray-100">
+      <div className="grid grid-cols-1 justify-items-center gap-5 bg-white border border-1 rounded-xl p-5 w-5/6 md:w-3/6">
+        <h1 className="text-xl font-bold">Caesar Cipher</h1>
+        <input
+          className="border rounded-xl p-3 outline-none focus:outline-[#349b5b] w-4/6"
+          type="text"
+          placeholder="Please Insert a Text to cipher."
+          value={normalText}
+          onChange={(event) => setNormalText(event.target.value)}
+        />
+        <input
+          className="border rounded-xl p-3 outline-none focus:outline-[#349b5b] w-4/6"
+          type="number"
+          value={key}
+          placeholder="Please Insert a Key to cipher."
+          onChange={(event) => onChange(event)}
+        />
+        <h2 className="text-lg font-bold">Ciphered Result</h2>
+        {cipheredString}
+      </div>
     </div>
   );
 }
